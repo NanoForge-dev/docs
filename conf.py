e991ee7e9c2754ec6b0640aead6bb1f8c50a16cb
MartinFillon <martin.fillon@hotmail.com>
2025-12-28 11:30:22 +0100

    docs: generate basic documentations

---
+++ conf.py
@@ -0,0 +1,9 @@
+# from cgitb import html
+
+# extensions = ["breathe"]
+
+html_theme = "sphinx_rtd_theme"
+
+# Breathe configuration
+# breathe_projects = {"ecs-lib": "doc_out/xml"}
+# breathe_default_project = "ecs-lib"
---
+++ gh-pages/_sources/index.rst.txt
@@ -0,0 +1,8 @@
+Nanoforge Documentation
+=======================
+
+.. toctree::
+  :maxdepth: 2
+
+this is a basic documentation project
+
