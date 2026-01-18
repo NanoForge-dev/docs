8d0c27f3818edc16b44dd47e14502fd552680c24
MartinFillon <martin.fillon@hotmail.com>
2026-01-18 11:37:40 +0100

    feat(conf): add project name

--- conf.py
+++ conf.py
@@ -5,3 +5,11 @@ html_theme = "sphinx_rtd_theme"
 # Breathe configuration
 breathe_projects = {"ecs-lib": "engine/docs/registry/api/xml"}
 breathe_default_project = "ecs-lib"
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".txt": "markdown",
+    ".md": "markdown",
+}
+
+project = "Nanoforge"
